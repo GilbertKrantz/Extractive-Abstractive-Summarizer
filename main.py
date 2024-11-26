f60bf19826f4e1c1e127082bc41a962e3ce6b748
GilbertKrantz <wilbertchandra.official@gmail.com>
2024-11-26 13:22:06 +0700

    Streamlit Main File

---
+++ main.py
@@ -0,0 +1,24 @@
+import Abstractive_Summarization.abstractive_summarizer as abstractive_summarizer
+import Extractive_Summarization.extractive_summarizer as extractive_summarizer
+import streamlit as st
+
+def main():
+    # Get user text input
+    text = st.text_area("Enter the text you want to summarize: ")
+    # Get user choice of summarization method
+    summarization_method = st.selectbox("Choose the summarization method", ("Abstractive", "Extractive"))
+    
+    if summarization_method == "Abstractive":
+        # Summarize the text using abstractive summarization
+        max_length = text.count(' ') + 1
+        text_handler = abstractive_summarizer.textHandler(max_length=max_length)
+        summary = text_handler.summarize(text)
+        st.write(summary)
+    elif summarization_method == "Extractive":
+        # Summarize the text using extractive summarization
+        text_handler = extractive_summarizer.textHandler()
+        summary = text_handler.summarize(text)
+        st.write(summary)
+        
+if __name__ == "__main__":
+    main()
